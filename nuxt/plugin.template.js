8bc51dd43f299640ad6b8260317c69484a1433e3
Penguin <say8425@gmail.com>
2020-02-25 16:16:59 +0900

    nuxt plugin template 에도 options.scriptUrl 반영

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -4,5 +4,7 @@ import VueDaumPostcode from "vue-daum-postcode"
 
 const options = {}
 <% if (options.name) { %>options.name = "<%= options.name %>"<% } %>
+<% if (options.scriptUrl) { %>options.scriptUrl = "<%= options.scriptUrl %>"<% } %>
+
 
 Vue.use(VueDaumPostcode, options)
